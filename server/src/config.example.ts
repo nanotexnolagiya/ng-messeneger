6a32c994cee984678aae2a7a6a65ed2c8c62c377
Islomkhuja <nanotexnolagiya@gmail.com>
2020-07-22 18:09:37 +0500

    fix config example

--- server/src/config.example.ts
+++ server/src/config.example.ts
@@ -3,15 +3,36 @@ const configs = {
     "host": "localhost",
     "port": "3030",
     "apiVersion": "v1",
-    "mongoDbUri": "mongodb://localhost:27017/dbName",
-    "secret": "THE_SECRET_KEY"
+    "publicPath": "public",
+    "uploadsPath": "uploads",
+    "mongoDbUri": "MONGO_SERVER_URL",
+    "secret": "YOUR_SECRET_KEY",
+    "smtp": {
+      "host": "YOUR_SMTP_HOST",
+      "port": 587, // 465, 587, 25
+      "secure": false,
+      "auth": {
+        "user": "YOUR_SMTP_USERNAME",
+        "pass": "YOUR_SMTP_USER_PASSWORD"
+      }
+    }
   },
   "production": {
     "host": "localhost",
-    "port": "3030",
+    "port": "3000",
     "apiVersion": "v1",
-    "mongoDbUri": "mongodb://localhost:27017/dbName",
-    "secret": "THE_SECRET_KEY"
+    "mongoDbUri": "MONGO_SERVER_URL",
+    "secret": "YOUR_SECRET_KEY",
+    "smtp": {
+      "host": "YOUR_SMTP_HOST",
+      "port": 587, // 465, 587, 25
+      "secure": false,
+      "auth": {
+        "user": "YOUR_SMTP_USERNAME",
+        "pass": "YOUR_SMTP_USER_PASSWORD"
+      }
+    }
   }
 }
+
 export default configs[process.env.NODE_ENV ? process.env.NODE_ENV : 'development']
